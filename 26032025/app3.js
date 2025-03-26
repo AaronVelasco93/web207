7bfd1929ca2ccf44e2c18e5e3b710a20c782d3ea
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-03-26 12:03:33 -0600

    clase 26032025 v1

---
+++ 26032025/app3.js
@@ -0,0 +1,35 @@
+//Inicializamos un arreglo para almacenar las tareas
+let tareas = [];
+
+//Funcion para mostrar un menu
+
+function mostrarMenu(){
+   return parseInt( prompt(
+    "Opciones disponibles: \n"+
+    "1. Agregar tarea\n"+
+    "2. Ver todas las tareas\n"+
+    "3. Marcar una tarea como completada\n"+
+    "4. salir\n"+
+    "Elige una opcion: "
+));
+}
+
+function agregarTarea(){
+    console.log("En proceso");
+}
+function verTareas(){
+    console.log("En proceso");
+}
+function marcarTareaCompletada(){
+    console.log("En proceso");
+}
+
+//Funcion pricipal para menejar el programa
+
+function  iniciarPrograma(){
+
+}
+
+
+//Iniciar programa
+iniciarPrograma();
