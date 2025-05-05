8cc1de0fffd832c1db5cdd3f6df18f18c6440110
AaronVelasco93 <aaronvelasco1993@outlook.com>
2025-05-05 12:32:19 -0600

    clase del 05052025

---
+++ 05052025/appPath.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+//creamos una instancia
+const app=express();
+
+//puerto
+const port=3089;
+
+
+
+//menejador de la solicitudes
+app.get("/",(req,res)=>{
+    //res.send("Hola mundo");
+    //res.sendFile("c:/Users/aaron/Escritorio/clases2025/web207/05052025/index.html");
+    res.sendFile( path.join(__dirname+"/index.html"));
+});
+//Salida del servidor
+app.listen(port,()=>{
+    console.log(`http://127.0.0.1:${port}`);
+});
